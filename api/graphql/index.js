b5ce6c004129a2c31b880ad0c9da0d750dbbe916
Kent Ball <kent.vincent.ball@gmail.com>
2025-04-10 18:24:39 -0400

    Updated package.json and main GraphQL handlers

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -1,40 +1,64 @@
 const { ApolloServer } = require('@apollo/server');
-const { startServerAndCreateHandler } = require('@apollo/server-integration-vercel');
 const { typeDefs } = require('../../server/schema/typeDefs');
 const resolvers = require('../../server/schema/resolvers');
 const prisma = require('../../server/utils/prisma');
 const { checkAuth } = require('../../server/utils/auth');
 require('dotenv').config();
 
-// Create the Apollo Server
+// Create Apollo Server instance
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: true, // Enable introspection in all environments
-  cache: 'bounded',
-  formatError: (error) => {
-    console.error('GraphQL error:', error);
-    
-    // Return custom error format
-    return {
-      message: error.message,
-      locations: error.locations,
-      path: error.path,
-      extensions: error.extensions,
-    };
-  },
+  introspection: true
 });
 
-// Create and export the handler
-module.exports = startServerAndCreateHandler(server, {
-  context: async ({ req }) => {
-    // Get user from token
-    const user = checkAuth({ req });
-    
-    // Return the context object
-    return {
-      user,
-      prisma,
-    };
-  },
-});
+// Start the server (only done once)
+const startServerPromise = server.start();
+
+// Export the handler function
+module.exports = async (req, res) => {
+  // Ensure server is started
+  await startServerPromise;
+  
+  // For CORS preflight requests
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.status(204).end();
+  }
+  
+  // Handle requests
+  if (req.method === 'POST') {
+    try {
+      // Get user from authentication
+      const user = checkAuth({ req });
+      
+      // Execute query
+      const { body } = req;
+      const response = await server.executeOperation({
+        query: body.query,
+        variables: body.variables,
+        operationName: body.operationName,
+      }, {
+        contextValue: {
+          user,
+          prisma
+        },
+      });
+
+      // Return response
+      return res.status(200).json(response);
+    } catch (error) {
+      console.error('GraphQL error:', error);
+      return res.status(500).json({
+        errors: [{ message: 'Internal server error' }]
+      });
+    }
+  } else {
+    // Only POST is allowed for GraphQL
+    return res.status(405).json({
+      errors: [{ message: 'Method not allowed' }]
+    });
+  }
+};
